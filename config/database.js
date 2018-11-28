f37fc920c365bfd3ce88ac1cac3bf988e280e9b9
Stephan Kaag <stephan@ka.ag>
2018-11-28 14:58:34 +0000

    Config via env variables

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   hrPool: {
-    user: 'system',
-    password: 'mn*sk[45.uo',
-    connectString: "192.168.2.114/ORCL.LOCAL", // find service-name with: select * from global_name;
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    connectString: process.env.CONNECTSTRING, // For example: "192.168.2.114/ORCL.LOCAL" find service-name with: select * from global_name;
     poolMin: 10,
     poolMax: 10,
     poolIncrement: 0
